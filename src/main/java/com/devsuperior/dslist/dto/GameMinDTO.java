b80b5844426a59e81716011ede6753ee02ab39b9
KelvinNatal <kelvinnatal@hotmail.com>
2023-05-09 21:06:11 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -2,16 +2,17 @@ package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class GameMinDTO {
     private Long id;
     private String title;
     private Integer year;
     private String imgUrl;
     private String shortDescription;
-
-    public GameMinDTO(){
-
-    }
 
     public GameMinDTO(Game entity) {
         id = entity.getId();
@@ -20,25 +21,4 @@ public class GameMinDTO {
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public String getImgUrl() {
-        return imgUrl;
-    }
-
-    public String getShortDescription() {
-        return shortDescription;
-    } 
-    
 }
---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+    
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+
+    private Integer position;
+
+    public Belonging(Game game, GameList list, Integer position){
+        id.setGame(game);
+        id.setGameList(list);
+        this.position = position;
+    }
+}
