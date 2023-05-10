c2884db0da3ba55f756836bce3a69b4c89b9566b
KelvinNatal <kelvinnatal@hotmail.com>
2023-05-09 22:00:00 -0300

    GET games by lists

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,4 +22,12 @@ public class GameMinDTO {
         imgUrl = entity.getImgUrl();
         shortDescription = entity.getShortDescription();
     }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
+    } 
 }
