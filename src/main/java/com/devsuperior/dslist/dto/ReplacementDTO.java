ea765db12a0c8d8373178ad75cf8d290bd587804
KelvinNatal <kelvinnatal@hotmail.com>
2023-05-13 12:45:10 -0300

    Move games on List

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslist.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReplacementDTO {
+
+    private int sourceIndex;
+    private int destinationIndex;
+}
